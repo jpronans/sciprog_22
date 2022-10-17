50f0a9fa5c2775bb3be4817c400691d1fb7b023c
John Ronan <noreply>
2022-10-17 22:55:48 +0100

    Working version of first half of practical5

--- Practical_05/practical05.c
+++ Practical_05/practical05.c
@@ -1,10 +1,54 @@
 #include <stdio.h>
 
+void calc(int *fn1, int *fn2)
+{
+  int next;
+  // Fn = Fn-1 + Fn-2
+  next = (*fn1) + (*fn2);
+
+  // Remember to copy original before overwriting
+  (*fn2)=(*fn1);
+  (*fn1)=next;
+}
+
 int main(void) {
-  int i; double a;
+  
+  // For loop counter
+  int i;
+  // Number of terms
+  int n;
+  // Initialse first two terms
+  int f0 = 0;
+  int f1 = 1;
+    
+
   // Get information from user
-  printf("please enter an int and double\n");
-  scanf("%d %lf",&i,&a);
-  printf("Int = %d, Double = %lf\n",i,a);
+  printf("Can I have an 'n' please (int)\n");
+  scanf("%d",&n);
+
+  printf("Fibonacci series is: \n");
+  if (n == 0)  {
+    // print first one
+    printf("%d", f0);
+  }
+  else if (n == 1)
+  { 
+    // Print first two 
+    printf("%d %d", f0, f1);
+  }
+  else {
+    // Print first two
+    printf("%d %d", f0, f1);
+    
+    // Continue to print the rest of the series requested up to 'n'
+    for (i = 2; i <=n; i++){
+
+      calc(&f1, &f0);
+      printf(" %d", f0);
+      
+    }
+    printf("\n");  
+  }
+  // No issues, exit cleanly
   return 0;
 }

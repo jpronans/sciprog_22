145a7ab2308c54c93d83b93b044d56bb9b022629
John Ronan <noreply>
2022-11-03 21:11:18 +0000

    More comments

--- Practical_08/gcda.c
+++ Practical_08/gcda.c
@@ -1,28 +1,37 @@
 #include <stdio.h>
 
-// Main
+// Solve for Greatest Common Divisor using loop method 
+// Can't quite visualise it though 
 int main(void)
 {
-  // Two numbers
+  // A variable per number
   int a, b;
 
   printf("Please enter 'A'\n");
   scanf("%d", &a);
+
+  // Small bit of defensive programming, fail early
   if (a < 1){
-    printf("To small, try again");
+    printf("Non zero please, try again\n");
     return 0;
   }
 
   printf("Please enter 'B'\n");
   scanf("%d", &b);
+  
+  // Small bit of defensive programming 
   if (b < 1){
-    printf("To small, try again");
+    printf("Non zero please, try again\n");
     return 0;
   }
-  int temp;
+  int temp = 0;
   while ( b != 0){
+    // Store b
     temp = b;
+    // B is the remainder of a divided by b range[0, b-1)
     b = a % b;
+
+    // Restore for next run through  
     a = temp; 
   }
   printf("GCD is %d\n", a); 

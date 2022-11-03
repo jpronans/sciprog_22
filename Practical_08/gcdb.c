debc4653676f3c2f41c88e8fc82aad90db889ed3
John Ronan <noreply>
2022-11-03 21:23:44 +0000

    Remove while loop from previous version, engage gcd function and added some comments

--- Practical_08/gcdb.c
+++ Practical_08/gcdb.c
@@ -4,40 +4,40 @@
 int gcd (int a, int b);
 
 // Find the Greatest Common Divisor of a pair of Integers using recursion
+// Think I get it now, smaller/bigger gives bigger as remainder, so it just swaps 
+// a and b and continues on.
 int main(void)
 {
   // Two numbers
   int a, b;
-
+  
   printf("Please enter 'A'\n");
   scanf("%d", &a);
   if (a < 1){
-    printf("To small, try again");
+    printf("Non zero please, try again\n");
     return 0;
   }
 
   printf("Please enter 'B'\n");
   scanf("%d", &b);
   if (b < 1){
-    printf("To small, try again");
+    printf("Non zero please, try again\n");
     return 0;
   }
-  int temp;
-  while ( b != 0){
-    temp = b;
-    b = a % b;
-    a = temp; 
-  }
-  printf("GCD is %d\n", a); 
+  
+  printf("GCD is %d\n", gcd(a,b)); 
 
   // Exit cleanly
   printf("Done!\n");
   return 0;
 }
 
+// Slightly more headwrecking, but keep going until we hit zero, then unroll 
 int gcd (int a, int b) {
-  if (b != 0 )
+  if (b != 0 ){
     return gcd(b, a % b);
-  else
+  }
+  else {
     return a;
+  }
 }

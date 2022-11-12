784d1485712d3b9771d619129c1c03f14755dbd7
John Ronan <noreply>
2022-11-12 20:19:10 +0000

    Writing test case to file

--- Practical_09/main.c
+++ Practical_09/main.c
@@ -29,23 +29,36 @@ int main(void)
 {
 
   // Counter variables
-  int i,j,k,p,q,mid,order,res;
-  int* magic;
- 
+  //int i,j,k,p,q,mid,order,res;
+  //int* magic;
+
+  int i,r,c;
+   
   printf("Initial Test of 3x3\n");
   
   int three1[3]= {2,7,6};
-  int three2[4]= {9,5,1};
+  int three2[3]= {9,5,1};
   int three3[3]= {4,3,8};
 
   int *newthree[3] = {three1,three2,three3};
 
   
+  // Check return value of isMagicSquare  
+  if (isMagicSquare(newthree, 3) == 1)
+    printf("Matrix is Magic\n");
+
+  FILE* fp=fopen("test","w");
   
-  res = isMagicSquare(newthree, 3);
-  if (res == 0)
-    printf("Not Magic");
- 
+  for(r = 0; r < 3; r++){
+    for(c = 0; c < 3; c++){
+      fprintf(fp, "%d", newthree[r][c]); 
+    }
+    fprintf(fp, "\n");
+  }
+  fclose(fp);
+
+ // fp = fopen("test","r");
+
   return 0;
 
 }

46c5e108a39c1a863f529c26a371c7569c86092f
John Ronan <noreply>
2022-11-20 22:16:37 +0000

    Code runs without crashing, but not happy its working correctly

--- Practical_10/main.c
+++ Practical_10/main.c
@@ -19,7 +19,7 @@ struct Stack* createStack(const int capacity)
 {
 	struct Stack* s =	(struct Stack*) malloc(sizeof(struct Stack));
 	s->capacity = capacity;
-  // Init to 'bad' value
+  // Init to 'sentinel' value
 	s->top = -1;
   // Allocate space for array of ints
 	s->array = (int*) malloc(s->capacity * sizeof(int));
@@ -28,7 +28,8 @@ struct Stack* createStack(const int capacity)
 
 // Stack is full when top is equal to the last index
 int isFull(struct Stack* stack)
-{
+{ 
+  // One less than the capcaity = FULL 
   return (stack->top == stack->capacity - 1);
 }
 
@@ -38,8 +39,7 @@ int isEmpty(struct Stack* stack)
   return (stack->top == -1);
 }
 
-// Function to add an item to stack. It increases
-// top by 1
+// Function to add an item to stack. It increases top by 1
 void push(struct Stack *stack, int item)
 { 
 	if (isFull(stack))
@@ -47,6 +47,8 @@ void push(struct Stack *stack, int item)
     printf("Overflow adding %d to stack. Not added",item);  
 		return;
   }
+  // Store the item into the top of the stack(pre-increment)
+  stack->array[++stack->top] = item;
 }
 
 // Function to remove an item from stack. It
@@ -55,37 +57,137 @@ int pop(struct Stack* stack)
 {
   // Underflow
 	if (isEmpty(stack))
-		return -1;
-
+		return -9;
+  // Return 
+  return stack -> array[stack -> top--];
 }
 
+//Function to show the movement of disks
+void showMove(char from, char to, int disk)
+{
+    printf("Move disk %d from:%c to :%c\n\n", disk, from, to);
+}
 
 // Function to implement legal movement between two poles
-void moveDisks(struct Stack *src, struct Stack *dest, char s, char d)
+void swapDisks(struct Stack *src, struct Stack *dest, char s, char d)
 {
-
+    // Get the two top values
+    int pole1Top = pop(src);
+    int pole2Top = pop(dest);
+ 
+    // When source pole is empty
+    if (pole1Top == -9)
+    {
+        //if src is empty, move top of dest to it.        
+        push(src, pole2Top);
+        showMove(d, s, pole2Top);
+    }
+ 
+    // When dest pole is empty move top of src to it.
+    else if (pole2Top == -9)
+    {
+        push(dest, pole1Top);
+        showMove(s, d, pole1Top);
+    }
+ 
+    // When top disk of src > top disk of dest, push them to src in right order
+    else if (pole1Top > pole2Top)
+    {
+        push(src, pole1Top);
+        push(src, pole2Top);
+        showMove(d, s, pole2Top);
+    }
+ 
+    // When top disk of src < top disk of dest, push them to dest in right order
+    else
+    {
+        push(dest, pole2Top);
+        push(dest, pole1Top);
+        showMove(s, d, pole1Top);
+    }
 }
 
-//Function to implement Towers of Hanoi
-iterate(int no_of_disks, struct Stack *src, struct Stack *dest,	struct Stack *aux)
-{
-// Just do it!
+
+
+
+
+void create_and_test_stack(int no_of_disks){
+  struct Stack *temp;
+  int i=0;
+  int tmp=0;
+  temp=createStack(no_of_disks);
+  
+  for(i=0; i< no_of_disks;i++){
+    push(temp, i);
+  }
+  // print and pop
+  for(i=0; i< no_of_disks;i++){
+    tmp=pop(temp);
+    printf("Stored Value is %d\n",tmp);
+  }
+  printf("Test complete\n");
+
+  free(temp); 
 }
 
+void iterate(int no_of_disks, struct Stack *src, struct Stack *dest, struct Stack* spare){
 
-void create_and_test_stack(int no_of disks){
+  int i, tmp, total_num_of_moves;
+  char s = 'S', d = 'D', p = 'P'; // Source, Dest, sPare
 
+  // If number of disks is even, then interchange destination pole and auxiliary pole
+  if (no_of_disks % 2 == 0)
+  {
+    char temp = d;
+    d = p;
+    p = temp;
+  }
+  // Known value
+  total_num_of_moves = (no_of_disks*no_of_disks) - 1;
+
+
+  //Larger disks will be pushed first
+  for (i = no_of_disks; i >= 1; i--){
+    printf("Seeding stack posn %d with %d\n",i, i);
+    push(src, i);
+  }
+
+
+  // Looking at the video, it all comes down to the same few moves, again and again.
+  for (i = 1; i <= total_num_of_moves; i++)
+  {
+    if (i % 3 == 1){
+      printf("Swap src->item:%d, with dest->item:%d\n", src->top, dest->top);
+      swapDisks(src, dest, s, d);
+    }
+    else if (i % 3 == 2){ // swap src and spare
+      printf("Swap src->item:%d, with spare->item:%d\n", src->top, spare->top);
+      swapDisks(src, spare, s, p);
+    }
+
+    else if (i % 3 == 0){
+      printf("Swap spare->item:%d, with dest->top %d\n",spare->top, dest->top);
+      swapDisks(spare, dest, p, d);
+    }
+  }
+  // pop and print
+  int tmp1, tmp2;
+  for (i = no_of_disks; i >= 1; i--){
+     tmp=pop(src);
+     tmp1=pop(spare);
+     tmp2=pop(dest);
+
+     printf("Reading Stack posn %d, result %d:\t%d:\t%d\n",i,tmp, tmp1, tmp2);
+  }
+  //
 }
 
-void interate(int n, stack *src, stack *dest, stack* spare) 
-
-// Driver Program
-int main()
+// Attempt at iterative Tower of Hanoi solution 
+int main(void)
 {
-	// Input: number of disks
-	int no_of_disks = 0;
+	int no_of_disks = 0;  // user inputs number of disks
 
-	struct Stack *test, *src, *dest, *aux;
+	struct Stack *src, *dest, *spare;
 
   printf("Please enter the number of disks\n");
   scanf("%d", &no_of_disks);
@@ -97,8 +199,22 @@ int main()
   }
   create_and_test_stack(no_of_disks);
 
-// Iterate over the three poles.
-// iterate(no_of_disks, src, spare, dest);
-	return 0;
+  // Create three 'no_of_disks' sized stacks
+  src = createStack(no_of_disks);
+  dest = createStack(no_of_disks);
+  spare = createStack(no_of_disks);
+
+
+  // Iterate over the three poles.
+  iterate(no_of_disks, src, dest, spare);
+
+  printf("All done\n");
+
+  // Free up what we allocated with createStack
+  free(src);
+  free(dest);
+  free(spare);  
+	// exit cleanly
+  return 0;
 }
 

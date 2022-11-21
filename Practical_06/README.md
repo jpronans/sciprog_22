f3a7b7fadef0510b3fe92ba161a5487fef44f14e
John Ronan <noreply>
2022-11-21 21:26:10 +0000

    Added README and made some changes to improve readability

---
+++ Practical_06/README.md
@@ -0,0 +1,15 @@
+# Instructions for practical
+
+## To build
+make clean; make
+
+## To Run
+./mmc
+
+Multiplies two matrices together
+
+You will be asked for the number of disks you wish to simulate
+
+# Notes
+* For some reason I thought structs were required in the answer, hence I used a struct. 
+* Maxmult function is in mm.c with prototypes in mm.h 
--- Practical_06/mm.c
+++ Practical_06/mm.c
@@ -11,14 +11,14 @@ struct c_matrix matmult(const struct a_matrix am, const struct b_matrix bm, stru
   int sum;
   int n,p,q;
   printf("Inside matmult\n");
-  printf("A Matrix -\n");
+  printf("A Matrix --\n");
   for (i=0; i<N; i++){
     for (j=0; j<P; j++){
       printf("%3d", am.values[i][j]);
     }
     printf("\n");
   }
-  printf("----------\n\n");
+  printf("-----------\n\n");
 
   // Print B
   printf("B Matrix ----\n");
@@ -33,13 +33,16 @@ struct c_matrix matmult(const struct a_matrix am, const struct b_matrix bm, stru
   // No of rows of first matrix N='5'
   // Easier to understand using the same variable names as the practical document
   for (n=0; n < N; n++){
+    
     // No of Columns in second matrix Q='4'
     for (q=0; q < Q ; q++){
+      
       // No of Rows in Second matrix P='3'
       // Only can work if columns in first and rows in second matrix are equal
       for (p=0; p < P; p++){
           sum = sum +  am.values[n][p] * bm.values[p][q];
       }
+      
       // Take the result and store it in the correct location in 'C'
       cm.values[n][q] = sum;
       sum = 0;
